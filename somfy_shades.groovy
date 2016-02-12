44803dac7f8c0104cc76f625833ae7e24444915f
imnotbob <Imnot_bob@hotmail.com>
2016-02-12 08:33:04 -0500

    Added Window Shade capability
    Updated colors for legibility

--- somfy_shades.groovy
+++ somfy_shades.groovy
@@ -1,18 +1,22 @@
 /**
  * 
  * https://community.smartthings.com/t/my-somfy-smartthings-integration/13492
- * Modified ERS 1/28/2016
+ * Modified ERS 2/12/2016
  * Changes:
- *	Shows if shades are in a my/stop state
- *	Multi-tile control
- *	Disable dual setLevel operations
+ *        Shows if shades are in a my/stop state
+ *        Multi-tile control
+ *        Disable dual setLevel operations
  */
   metadata {
     definition (name: "Somfy Z-Wave Shades Multi tile", namespace: "ash", author: "Ash Raj") {
         capability "Switch Level"
         capability "Switch"
+        capability "Window Shade"
+        //capability "Polling"
+        capability "Refresh"
+        capability "Actuator"
 
-	command "levelOpenClose"
+        command "levelOpenClose"
 
         fingerprint deviceId: "0x1105", inClusters: "0x2C, 0x72, 0x26, 0x20, 0x25, 0x2B, 0x86"
     }
@@ -37,32 +41,34 @@
 
 
     tiles(scale: 2) {
-	multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4){
-		tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-		    attributeState("on", label:'open', action:"switch.off", icon:"st.doors.garage.garage-open", backgroundColor:"#ffdf3f")
-		    attributeState("off", label:'closed', action:"switch.on", icon:"st.doors.garage.garage-closed", backgroundColor:"#194775")
-		    attributeState("default", label:'stop/my', action:"switch level.setLevel", icon:"st.doors.garage.garage-open", backgroundColor:"#ffdf3f")
-		}
-		tileAttribute ("device.level", key: "SLIDER_CONTROL") {
-				attributeState("level", action:"switch level.setLevel")
-		}
-		tileAttribute ("device.speedLevel", key: "VALUE_CONTROL"){
-                   attributeState("level", action: "levelOpenClose")
-                 }
-	}
-
+        multiAttributeTile(name:"shade", type: "lighting", width: 6, height: 4) {
+            tileAttribute("device.windowShade", key: "PRIMARY_CONTROL") {
+                attributeState("unknown", label:'${name}', action:"refresh.refresh", icon:"st.doors.garage.garage-open", backgroundColor:"#ffa81e")
+                attributeState("closed", label:'${name}', action:"open", icon:"st.doors.garage.garage-closed", backgroundColor:"#bbbbdd", nextState: "opening")
+                attributeState("open", label:'${name}', action:"close", icon:"st.doors.garage.garage-open", backgroundColor:"#ffcc33", nextState: "closing")
+                attributeState("partially open", label:'stop/my', action:"presetPosition", icon:"st.doors.garage.garage-open", backgroundColor:"#ffcc33")
+                attributeState("closing", label:'${name}', action:"presetPosition", icon:"st.doors.garage.garage-closing", backgroundColor:"#bbbbdd")
+                attributeState("opening", label:'${name}', action:"presetPosition", icon:"st.doors.garage.garage-opening", backgroundColor:"#ffcc33")
+            }
+            tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+                attributeState("level", action:"switch level.setLevel")
+            }
+            tileAttribute ("device.speedLevel", key: "VALUE_CONTROL") {
+                attributeState("level", action: "levelOpenClose")
+            }
+        }
 
         standardTile("switchmain", "device.switch", width: 2, height: 2) {
-            state "on", label:'open', action:"switch.off", icon:"st.doors.garage.garage-open", backgroundColor:"#ffdf3f"
-            state "off", label:'closed', action:"switch.on", icon:"st.doors.garage.garage-closed", backgroundColor:"#194775"
-            state "default", label:'stop/my', action:"switch level.setLevel", icon:"st.doors.garage.garage-open", backgroundColor:"#ffdf3f"
+            state "on", label:'open', action:"switch.off", icon:"st.doors.garage.garage-open", backgroundColor:"#ffcc33"
+            state "off", label:'closed', action:"switch.on", icon:"st.doors.garage.garage-closed", backgroundColor:"#bbbbdd"
+            state "default", label:'stop/my', action:"presetPosition", icon:"st.doors.garage.garage-open", backgroundColor:"#ffcc33"
         }
 
         standardTile("on", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-            state("on", label:'up', action:"switch.on", icon:"st.doors.garage.garage-opening")
+            state("on", label:'open', action:"switch.on", icon:"st.doors.garage.garage-opening")
         }
         standardTile("off", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-            state("off", label:'down', action:"switch.off", icon:"st.doors.garage.garage-closing")
+            state("off", label:'close', action:"switch.off", icon:"st.doors.garage.garage-closing")
         }
         standardTile("stop", "device.level", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
             state("default", label:'stop/my', action:"switch level.setLevel", icon:"st.Transportation.transportation13")
@@ -71,8 +77,18 @@
             state("level", action:"switch level.setLevel")
         }
 
+        standardTile("refresh", "command.refresh", width:2, height:2, inactiveLabel: false, decoration: "flat") {
+                state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
+
+//  Poll provides data, but the ZRTSII does not provide accurate status
+//
+//      standardTile("poll", "command.poll", width:2, height:2, inactiveLabel: false, decoration: "flat") {
+//              state "default", label:'poll', action:"poll", icon:"st.secondary.poll"
+//      }
+
         main(["switchmain"])
-        details(["switch", "on", "off", "stop"])
+        details(["shade", "on", "off", "stop"])
     }
 }
 
@@ -93,91 +109,176 @@ def parse(String description) {
 def levelOpenClose(value) {
     log.trace "levelOpenClose called with value $value"
     if (value) {
-	on()
+        on()
     } else {
-	off()
+        off()
     }
 }
 
+// Somfy ZRTSII does not report accurate status for the device.
+// This device handler maintains an internal view of device status based on last command
+// reissuing a command to the shade (up, down, my (when stopped)) does not move the shade if it is already in that position
+// My/stop command does different actions depending if the shade is idle (go to MY position) or moving (stop)
+
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
 {
-    def result
-    log.debug "Basic report cmd.value:  ${cmd.value}"
+    def result = []
+    log.trace "Basic report cmd.value:  ${cmd.value}"
     if (cmd.value == 0) {
-        //result = createEvent(name: "switch", value: "off")
-    } else {
-        //result = createEvent(name: "switch", value: "on")
+        //result << createEvent(name: "switch", value: "off")
+        log.debug "Reported state is closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
+    } else if (cmd.value == 0xFF) {
+        //result << createEvent(name: "switch", value: "on")
+        log.debug "Reported state is open; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
+    } else {  // This has never happend
+        //result << createEvent(name: "switch", value: "default")
+        log.debug "Reported state is neither open or closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
     }
+    return result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+    def result = []
+    log.debug "SwitchBinaryReport cmd.value:  ${cmd.value}"
+    
+    if (cmd.value == 0) {
+        log.debug "Reported state is closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
+    } else if (cmd.value == 0xFF) {
+        log.debug "Reported state is open; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
+    } else {  // this has never happened
+        log.debug "Reported state is neither open or closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
+    }
+    
+    //result << createEvent(name:"switch", value: cmd.value ? "on" : "off")
+    //result << createEvent(name: "level",value: cmd.value, unit:"%",
+        //descriptionText:"${device.displayName} dimmed ${cmd.value==255 ? 100 : cmd.value}%")
     return result
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.switchmultilevelv1.SwitchMultilevelReport cmd)
 {
-    def result
-    log.debug "Multilevel report cmd.value:  ${cmd.value}"
+    def result = []
+    log.trace "SwitchMultilevelReport cmd.value:  ${cmd.value}"
+    
     if (cmd.value == 0) {
-        result = createEvent(name: "switch", value: "off")
+        //result << createEvent(name: "switch", value: "off")
+        log.debug "Reported state is closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
     } else if (cmd.value == 0xFF) {
-        result = createEvent(name: "switch", value: "on")
+        //result << createEvent(name: "switch", value: "on")
+        log.debug "Reported state is open; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
     } else {
-	result = createEvent(name: "switch", value: "default")
+       //result << createEvent(name: "switch", value: "default")
+       log.debug "Reported state is neither open or closed; device is ${device.latestValue('switch')}  ${device.latestValue('level')} "
     }
+    //result << createEvent(name: "level",value: cmd.value, unit:"%",
+      //descriptionText:"${device.displayName} dimmed ${cmd.value==255 ? 100 : cmd.value}%")
     return result
 }
 
 def on() {
     int level = 100
-    log.debug "on()"
+    log.trace "on() treated as open()"
     setLevel(level) 
 }
 
 def off() {
     int level = 0
-    log.debug "off()"
+    log.trace "off() treated as close()"
     setLevel(level) 
 }
 
 def setLevel() {
-    log.debug "setLevel()"
+    log.trace "setLevel() treated as my/stop"
     setLevel(50) 
 }
 
+def open() {
+    log.trace "open()"
+    on()
+}
+
+def close() {
+    log.trace "close()"
+    off()
+}
+
+def presetPosition() {
+    log.trace "presetPosition() treated as my/stop"
+    setLevel()
+}
+
+def refresh() {
+    log.trace "refresh()"
+    delayBetween([
+        zwave.switchBinaryV1.switchBinaryGet().format(),
+        zwave.switchMultilevelV1.switchMultilevelGet().format(),
+        //zwave.meterV2.meterGet(scale: 0).format(),      // get kWh
+        //zwave.meterV2.meterGet(scale: 2).format(),      // get Watts
+        //zwave.sensorMultilevelV1.sensorMultilevelGet().format(),
+        //zwave.sensorMultilevelV5.sensorMultilevelGet(sensorType:1, scale:1).format(),  // get temp in Fahrenheit
+        //zwave.batteryV1.batteryGet().format(),
+        zwave.basicV1.basicGet().format(),
+    ], 3000)
+}
+
+// If you add the Polling capability to your device type, this command
+// will be called approximately every 5 minutes to check the device's state
+// zrtsII does not provide accurate status of shade position
+//def poll() {
+//        log.trace "Poll"
+//        zwave.basicV1.basicGet().format()
+//}
+
 def setLevel(level) {
-    log.debug "setLevel(level)  {$level}"
+    log.trace "setLevel(level)  {$level}"
+    log.debug "level.inspect " + level.inspect()
+
+    int newlevel = level
 
     if (level > null) {
 
-	sendEvent(name: "level", value: level)
+        sendEvent(name: "level", value: level)
 
-	if (level >= 75) {
-	    delayBetween([
-		zwave.switchMultilevelV1.switchMultilevelSet(value: 0xFF).format(),
-		sendEvent(name: "switch", value: "on")
-	    ], 5000)
-	} else if (level <= 25) {
-	    delayBetween([
-    		zwave.switchMultilevelV1.switchMultilevelSet(value: 0x00).format(),
-    		sendEvent(name: "switch", value: "off")
-	    ], 5000)
-	} else {
-	    delayBetween([
-		zwave.switchMultilevelV1.switchMultilevelStopLevelChange().format(),
-		sendEvent(name: "switch", value: "default")
-	    ], 5000)
-	}
+        if (level >= 75) {
+            sendEvent(name: "windowShade", value: "opening")
+            newlevel = 100
+            delayBetween([
+                zwave.switchMultilevelV1.switchMultilevelSet(value: 0xFF).format(),
+                sendEvent(name: "switch", value: "on"),
+                sendEvent(name: "windowShade", value: "open")
+            ], 5000)
+        } else if (level <= 25) {
+            sendEvent(name: "windowShade", value: "closing")
+            newlevel = 0
+            delayBetween([
+                zwave.switchMultilevelV1.switchMultilevelSet(value: 0x00).format(),
+                sendEvent(name: "switch", value: "off"),
+                sendEvent(name: "windowShade", value: "closed")
+            ], 5000)
+        } else {
+            delayBetween([
+                zwave.switchMultilevelV1.switchMultilevelStopLevelChange().format(),
+                sendEvent(name: "switch", value: "default"),
+                sendEvent(name: "windowShade", value: "partially open")
+            ], 5000)
+        }
+
+        // this code below causes commands not be sent/received by the Somfy ZRTSII - assume delayBetween is asynchronous...
+
+        //log.trace("finished level adjust")
+        //if (newlevel != level) { 
+            //log.trace("finished level adjust1")
+            //delayBetween([
+                //sendEvent(name: "level", value: newlevel)
+            //], 1000)
+        //}
     }
 }
 
-def setLevel(level, duration) {
-    log.debug "setLevel(level, duration)  {$level} ${duration}"
-    setLevel(level)
-    return
+// this appears to never be called
 
-    if (level >= 75) {
-        zwave.switchMultilevelV1.switchMultilevelSet(value: 0xFF).format()
-    } else if (level <= 25) {
-        zwave.switchMultilevelV1.switchMultilevelSet(value: 0x00).format()
-    } else {
-        zwave.switchMultilevelV1.switchMultilevelStopLevelChange().format()
-    }
-}
+//def setLevel(level, duration) {
+//    log.trace "setLevel(level, duration)  {$level} ${duration}"
+//    setLevel(level)
+//    return
+//}
